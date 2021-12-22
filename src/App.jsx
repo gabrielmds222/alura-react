ee9f4184bec60969c13ff22c2767ebe7a7c69d3c
Gabriel <gabrielmedsilva@outlook.com>
2021-12-22 12:02:09 -0300

    NoteList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import React from 'react';
+import NoteList from './components/NoteList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <>
         <form>
           <label> Título </label>
           <input type="text" />
           <textarea placeholder='Escreva sua nota...'></textarea>
+          <button>Criar Nota</button>
         </form>
-      </>
+        <NoteList/>
     </div>
   );
 }
---
+++ src/components/NoteList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NoteList () {
+    return (
+        <ul>
+            <li>
+                <section>
+                    <header>
+                        <h3> Titulo </h3>
+                    </header>
+                    <p>Escreva sua nota</p>
+                </section>
+            </li>
+        </ul>
+    );
+};
+
+export default NoteList;
